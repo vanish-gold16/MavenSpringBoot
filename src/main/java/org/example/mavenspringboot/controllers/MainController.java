b7436fbe55998010d6d0e9a334ddaeaefbb7ec7c
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-01-21 10:34:39 +0100

    feat: update MainController to serve home page

--- src/main/java/org/example/mavenspringboot/controllers/MainController.java
+++ src/main/java/org/example/mavenspringboot/controllers/MainController.java
@@ -3,14 +3,13 @@ package org.example.mavenspringboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Main page");
+        return "home";
     }
 }
