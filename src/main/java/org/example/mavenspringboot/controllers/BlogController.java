22da0783cf69a366d4a3e65c1c6da1b09647af0f
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-01-21 13:10:27 +0100

    feat: add BlogController and blog.html template

--- src/main/java/org/example/mavenspringboot/controllers/BlogController.java
+++ src/main/java/org/example/mavenspringboot/controllers/BlogController.java
@@ -1,4 +1,15 @@
 package org.example.mavenspringboot.controllers;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+
 }
