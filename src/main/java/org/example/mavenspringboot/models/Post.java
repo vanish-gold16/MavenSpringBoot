632667da90f06918e64e76dd5312973d819aede8
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-01-22 09:33:24 +0100

    feat: add autogenerated ID to Post entity

--- src/main/java/org/example/mavenspringboot/models/Post.java
+++ src/main/java/org/example/mavenspringboot/models/Post.java
@@ -1,12 +1,15 @@
 package org.example.mavenspringboot.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
