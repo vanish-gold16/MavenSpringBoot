9879c390bb9e6fb1396201ae6fe07aa9652de1a4
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-01-22 10:21:55 +0100

    feat: add title, fullText, and views fields to Post entity

--- src/main/java/org/example/mavenspringboot/models/Post.java
+++ src/main/java/org/example/mavenspringboot/models/Post.java
@@ -1,9 +1,6 @@
 package org.example.mavenspringboot.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Post {
@@ -12,4 +9,44 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "full_text")
+    private String fullText;
+
+    @Column(name = "views")
+    private int views;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
 }
