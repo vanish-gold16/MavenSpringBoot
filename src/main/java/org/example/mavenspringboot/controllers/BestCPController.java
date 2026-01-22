dccb40009468c15656864a4d4b82658748028915
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-22 20:48:05 +0100

    feat: update BestCPController to set title attribute and remove title from HentaiController

--- src/main/java/org/example/mavenspringboot/controllers/BestCPController.java
+++ src/main/java/org/example/mavenspringboot/controllers/BestCPController.java
@@ -1,14 +1,17 @@
 package org.example.mavenspringboot.controllers;
 
-import ch.qos.logback.core.model.Model;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BestCPController {
 
+
     @GetMapping("/best-cp")
     public String bestCPMain(Model model){
+        model.addAttribute("title", "BEST CP");
         return "best-cp";
     }
 
--- src/main/java/org/example/mavenspringboot/controllers/HentaiController.java
+++ src/main/java/org/example/mavenspringboot/controllers/HentaiController.java
@@ -1,5 +1,8 @@
 package org.example.mavenspringboot.controllers;
 
+import org.example.mavenspringboot.models.Post;
+import org.example.mavenspringboot.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HentaiController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/hentai")
     public String hentai(Model model){
-        model.addAttribute("title", "Hentai page");
+        Iterable<Post> posts = postRepository.findAll();
         return "hentai";
     }
 }
