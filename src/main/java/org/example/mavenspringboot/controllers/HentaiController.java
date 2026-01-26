e19f3ade37d2b7b254aceb22df98694a4cae6057
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-26 17:00:57 +0100

    feat: create hentaiDetails in HentaiController.java

--- src/main/java/org/example/mavenspringboot/controllers/HentaiController.java
+++ src/main/java/org/example/mavenspringboot/controllers/HentaiController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HentaiController {
@@ -34,4 +39,16 @@ public class HentaiController {
         postRepository.save(post);
         return "redirect:/hentai";
     }
+
+    @GetMapping("/hentai/{id}")
+    public String hentaiDetails(@PathVariable(value = "id") long id,
+                            Model model){
+        Optional<Post> post = postRepository.findById(id);
+        List<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+
+        return "hentai-details";
+    }
+
 }
