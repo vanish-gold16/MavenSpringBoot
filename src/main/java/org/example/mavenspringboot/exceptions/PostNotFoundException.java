46aa3413bd11f1f48599228f41fd223ca9fe432a
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-01-28 20:06:19 +0100

    feat: add newline for improved readability in PostNotFoundException

--- src/main/java/org/example/mavenspringboot/exceptions/PostNotFoundException.java
+++ src/main/java/org/example/mavenspringboot/exceptions/PostNotFoundException.java
@@ -3,6 +3,7 @@ package org.example.mavenspringboot.exceptions;
 public class PostNotFoundException extends RuntimeException {
 
     public PostNotFoundException(long id){
+
         super("Post not found: " + id);
     }
 
